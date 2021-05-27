aff19acb6ab961e9a4c07815462b7db6e509750c
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-05-27 12:41:29 +0100

    tests

--- test/solarfile/solarfile_test.clj
+++ test/solarfile/solarfile_test.clj
@@ -1,7 +1,21 @@
 (ns solarfile.solarfile-test
-  (:require [clojure.test :refer :all]
-            [solarfile.solarfile :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [solarfile.solarfile :as SUT]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn failed? [job-run] (= (:job-status job-run) :failed))
+(defn success? [job-run] (= (:job-status job-run) :succeeded))
+
+(deftest events-test
+  (testing "events without file-name, location and run-id result in failed
+            job run"
+    (is (failed? (SUT/process-file-event! {:dry-run true})))
+    (is (failed? (SUT/process-file-event! {:file-name "test.txt"
+                                           :dry-run true})))
+    (is (failed? (SUT/process-file-event! {:file-name "test.txt"
+                                           :location :local
+                                           :dry-run true})))
+    (is (success? (SUT/process-file-event! {:file-name "test.txt"
+                                            :location :embedded
+                                            :data "Hello World"
+                                            :run-id #uuid "7a9cc2b5-2771-4bfc-acd2-9965e9b8479f"
+                                            :dry-run true})))))
